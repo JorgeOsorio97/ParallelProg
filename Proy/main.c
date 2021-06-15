ee31b5c5efb4870ccc53dd75bd37e57badb7f220
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2021-06-15 15:38:43 -0500

    parallelize proces with mpi

--- Proy/main.c
+++ Proy/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-// #include <mpi.h>
+#include <mpi.h>
 #include <math.h>
 
 int solver(int *arreglo);
@@ -9,78 +9,88 @@ int solver(int *arreglo);
 int main(void)
 {
     // MPI
-    // int comm_sz;
-    // int my_rank;
-    // MPI_Init(NULL, NULL);
-    // MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-    // MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    int comm_sz;
+    int my_rank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    // if (comm_sz != 4)
-    // {
-    //     printf("Por ahora el programa solo se paraleliza en 4");
-    //     return 1;
-    // }
+
 
     int big_size = pow(2, 16);
-    printf("big_size: %d", big_size);
-    int all_options[big_size][16];
-    int counter = 0;
+    int aux_size = big_size / comm_sz;
 
-    for (int a = 0; a <= 1; a++)
-    { //1
-        int aux[16];
-        aux[0] = a;
-        for (int b = 0; b <= 1; b++)
-        { //2
-            aux[1] = b;
-            for (int c = 0; c <= 1; c++)
-            { //3
-                aux[2] = c;
-                for (int d = 0; d <= 1; d++)
-                { //4
-                    aux[3] = d;
-                    for (int e = 0; e <= 1; e++)
-                    { //5
-                        aux[4] = e;
-                        for (int f = 0; f <= 1; f++)
-                        { //6
-                            aux[5] = f;
-                            for (int g = 0; g <= 1; g++)
-                            { //7
-                                aux[6] = g;
-                                for (int h = 0; h <= 1; h++)
-                                { //8
-                                    aux[7] = h;
-                                    for (int i = 0; i <= 1; i++)
-                                    { //9
-                                        aux[8] = i;
-                                        for (int j = 0; j <= 1; j++)
-                                        { //10
-                                            aux[9] = j;
-                                            for (int k = 0; k <= 1; k++)
-                                            { //11
-                                                aux[10] = k;
-                                                for (int l = 0; l <= 1; l++)
-                                                { //12
-                                                    aux[11] = l;
-                                                    for (int m = 0; m <= 1; m++)
-                                                    { //13
-                                                        aux[12] = m;
-                                                        for (int n = 0; n <= 1; n++)
-                                                        { //14
-                                                            aux[13] = n;
-                                                            for (int o = 0; o <= 1; o++)
-                                                            { //15
-                                                                aux[14] = o;
-                                                                for (int p = 0; p <= 1; p++)
-                                                                { //16
-                                                                    aux[15] = p;
-                                                                    for (int z = 0; z < 16; z++)
-                                                                    {
-                                                                        all_options[counter][z] = aux[z];
+    MPI_Datatype int_arr_arr;
+    MPI_Type_vector(aux_size, 16, 32, MPI_INT, &int_arr_arr);
+    MPI_Type_commit(&int_arr_arr);
+    
+
+    if (comm_sz != 4)
+    {
+        printf("Por ahora el programa solo se paraleliza en 4");
+        return 1;
+    }
+    
+
+        int all_options[big_size][16];
+        int counter = 0;
+    // create all options
+        for (int a = 0; a <= 1; a++)
+        { //1
+            int aux[16];
+            aux[0] = a;
+            for (int b = 0; b <= 1; b++)
+            { //2
+                aux[1] = b;
+                for (int c = 0; c <= 1; c++)
+                { //3
+                    aux[2] = c;
+                    for (int d = 0; d <= 1; d++)
+                    { //4
+                        aux[3] = d;
+                        for (int e = 0; e <= 1; e++)
+                        { //5
+                            aux[4] = e;
+                            for (int f = 0; f <= 1; f++)
+                            { //6
+                                aux[5] = f;
+                                for (int g = 0; g <= 1; g++)
+                                { //7
+                                    aux[6] = g;
+                                    for (int h = 0; h <= 1; h++)
+                                    { //8
+                                        aux[7] = h;
+                                        for (int i = 0; i <= 1; i++)
+                                        { //9
+                                            aux[8] = i;
+                                            for (int j = 0; j <= 1; j++)
+                                            { //10
+                                                aux[9] = j;
+                                                for (int k = 0; k <= 1; k++)
+                                                { //11
+                                                    aux[10] = k;
+                                                    for (int l = 0; l <= 1; l++)
+                                                    { //12
+                                                        aux[11] = l;
+                                                        for (int m = 0; m <= 1; m++)
+                                                        { //13
+                                                            aux[12] = m;
+                                                            for (int n = 0; n <= 1; n++)
+                                                            { //14
+                                                                aux[13] = n;
+                                                                for (int o = 0; o <= 1; o++)
+                                                                { //15
+                                                                    aux[14] = o;
+                                                                    for (int p = 0; p <= 1; p++)
+                                                                    { //16
+                                                                        aux[15] = p;
+                                                                        for (int z = 0; z < 16; z++)
+                                                                        {
+                                                                            all_options[counter][z] = aux[z];
+                                                                        }
+
+                                                                        counter++;
                                                                     }
-
-                                                                    counter++;
                                                                 }
                                                             }
                                                         }
@@ -96,55 +106,42 @@ int main(void)
                 }
             }
         }
-    }
-    printf("\n");
-    printf("counter: %d\n", counter);
-
-    for (int i = 0; i < big_size; i++)
-    {
-        if (solver(all_options[i]))
-        {
-            printf("index: %d, is_good: %d \n", i, solver(all_options[i]));
-        }
-    }
-
-    // for (int j = 0; j < 15; j++)
-    // {
-    //     printf('%d - ', all_options[0][j]);
-    // }
-
-    // for(int i=0; i<big_size; i++){
-    //     if(i<10 || i>= big_size-10){
-    //         for(int j=0; j<16; j++){
-    //             printf('%d - ', all_options[i][j]);
-    //         }
-    //         printf('\n');
-    //     }
-    // }
 
     // if (my_rank == 0)
     // {
+    //     int counter = 0;
+
+        
+    //     printf("\n");
+    //     printf("counter: %d\n", counter);
+
+    //     for (int i = 1; i < comm_sz; i++)
+    //     {
+    //         MPI_Send(&all_options, 1, int_arr_arr, i, 0, MPI_COMM_WORLD);
+    //     }
     // }
-    // int test[16] = {
-    //     0,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    //     1,
-    // };
-    // int res = solver(test);
-    // printf("%d\n", res);
+    // else
+    // {
+        // int all_options[big_size][16];
+        // int counter = 0;
+        // MPI_Recv(&all_options, 1, int_arr_arr, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        for (int i = 0; i < aux_size; i++)
+        {
+            int position = (aux_size * my_rank) + i;
+            int res = solver(all_options[position]);
+
+            // for (int j = 0; j < 16; j++)
+            // {
+            //     printf("%d - ", all_options[position][j]);
+            // }
+
+            if(res){
+                printf("From rank %d index number %d solutions equals %d", my_rank, position, res);
+            printf("\n");
+            }
+        }
+    // }
 
     return 0;
 }
